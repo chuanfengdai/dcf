8781f535689d494f7d94ff671b1b4f4008907d65
chuanfengdai <dcf_3310@126.com>
2017-03-24 19:34:44 +0800

    更改

--- src/git_github/HelloWolrd.java
+++ src/git_github/HelloWolrd.java
@@ -7,6 +7,11 @@ public class HelloWolrd {
 		System.out.println("helloworld");
 		System.out.println("Hi wolrd");
 		System.out.println("Hi wolrd222");
+		read();
+	}
+
+	private static void read() {
+		System.out.println("00000");
 		
 	}
 
